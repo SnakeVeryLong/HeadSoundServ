5b6620e14cc96618c28be000aa4d2fa9b0f2d933
SnakeVeryLong <ermolenko220699@gmail.com>
2023-03-15 21:18:01 +0700

    First

---
+++ src/main/java/com/example/HeadSound/HeadSoundApplication.java
@@ -0,0 +1,13 @@
+package com.example.HeadSound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadSoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeadSoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HeadSound/HeadSoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeadSound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadSoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
