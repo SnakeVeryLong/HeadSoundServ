c0523d56b4d8829dd7c6b3d593594d80b0ad5978
SnakeVeryLong <ermolenko220699@gmail.com>
2023-03-19 21:26:15 +0700

    Thirty

--- src/main/java/com/example/HeadSound/HeadSoundApplication.java
+++ src/main/java/com/example/HeadSound/HeadSoundApplication.java
@@ -2,8 +2,15 @@ package com.example.HeadSound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableAuthorizationServer
+@EnableResourceServer
+@RestController
 public class HeadSoundApplication {
 
 		public static void main (String[]args){
@@ -15,6 +22,9 @@ public class HeadSoundApplication {
 				t.printStackTrace();
 			}
 
+		}
+	@RequestMapping(value = "/products")
+	public String getProductName() {
+		return "Honey";
 	}
-
 }
